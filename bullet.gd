bd40d1a8854e7487008a94ebac8f8b7861964b8f
Nichbush <nichbush@iu.edu>
2026-04-28 12:15:25 -0500

    Add files via upload

--- bullet.gd
+++ bullet.gd
@@ -2,14 +2,15 @@
 extends Area2D
 
 @export var speed = 600
+@export var damage: int = 10
 
 func _physics_process(delta):
 	position += transform.x * speed * delta # Moves the bullet forward relative to its rotation
 
 func _on_body_entered(body):
 	# If the thing we hit is the player, do nothing and don't disappear
-	if body is Player:
+	if body.name == "Player":
 		return
 	if body.has_method("take_damage"):
-		body.take_damage(10)
+		body.take_damage(damage)
 	queue_free() # Destroy bullet on impact
